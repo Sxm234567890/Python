7010d7b1ece422592b66ba27cb33f7f0f1b61f2c
大米 <113964815+Sxm234567890@users.noreply.github.com>
2025-01-13 17:41:34 +0800

    Delete 爬取百度的图片.py

--- 爬取百度的图片.py
+++
@@ -1,8 +0,0 @@
-import requests
-url='https://psstatic.cdn.bcebos.com/video/wiseindex/aa6eef91f8b5b1a33b454c401_1660835115000.png'
-resp=requests.get(url)
-
-#把图片保存到本地
-with open('logo.png','wb')as file:
-    file.write(resp.content)
-    
